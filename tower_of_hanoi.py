91677968d4b66966250b51493f6e3af61ca25c7a
Lonnie Yu <lonnieljyu@gmail.com>
2017-07-23 17:37:22 -0400

    Added script description.

---
+++ tower_of_hanoi.py
@@ -0,0 +1,20 @@
+import random
+import numpy
+
+"""
+tower_of_hanoi.py
+- Lonnie Yu
+
+Sets up a Tower of Hanoi game with the given input parameters.
+Makes random valid moves for T iterations.
+Calculates the center of mass at each iteration.
+Returns the center of mass mean and standard deviation (std).
+"""
+
+def Run_Tower_Of_Hanoi(number_of_stacks, number_of_disks):
+    pass
+
+# Main 
+if __name__ == "__main__":
+    pass
+    
