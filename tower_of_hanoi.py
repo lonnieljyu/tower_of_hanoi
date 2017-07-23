44e213594a3424bf21c0227c3f386eef03a13bd1
Lonnie Yu <lonnieljyu@gmail.com>
2017-07-23 18:32:31 -0400

    Built initial data structures.

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -11,10 +11,28 @@ Calculates the center of mass at each iteration.
 Returns the center of mass mean and standard deviation (std).
 """
 
-def Run_Tower_Of_Hanoi(number_of_stacks, number_of_disks):
-    pass
+def Run_Tower_Of_Hanoi(number_of_stacks, number_of_disks, number_of_iterations):
+    # Initialize data structures
+    stacks = [list() for i in range(number_of_stacks)]
+    stacks[0] = [weight for weight in reversed(range(1, number_of_disks+1))]
+    centers_of_mass = list()
+    
+    # Loop over T iterations
+    for t in range(1, number_of_iterations+1):
+    
+        # Build move set
+        
+        # Randomly choose move from move set
+        
+        # Calculate and append center of mass to list
+        
+        print("T: ", t)
+        
+    # Return center of mass mean and std
+    
+    print(stacks)
 
 # Main 
 if __name__ == "__main__":
-    pass
+    Run_Tower_Of_Hanoi(3, 3, 16)
     
