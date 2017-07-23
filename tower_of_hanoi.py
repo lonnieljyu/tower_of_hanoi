ebc0fdc90868192a2667512a4998d675f8d3df86
Lonnie Yu <lonnieljyu@gmail.com>
2017-07-23 19:33:51 -0400

    Added Get_Move_Set()

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -11,22 +11,57 @@ Calculates the center of mass at each iteration.
 Returns the center of mass mean and standard deviation (std).
 """
 
+# Returns of a set of move tuples (stack position, direction)
+def Get_Move_Set(stacks):
+    moves = set()
+    M = len(stacks) # number of stacks
+    
+    print("Get Move Set")
+    print(stacks)
+    for i in range(M):
+        print("i:", i)
+        print(stacks[i])
+        
+        current_stack = stacks[i]
+        if(current_stack):
+        
+            # Add left move if valid 
+            if (i > 0 and (not stacks[i-1] 
+                            or current_stack[-1] < stacks[i-1][-1])):
+                moves.add((i, -1))
+            
+            # Add right movie if valid 
+            if (i < M - 1 and (not stacks[i+1] 
+                                or current_stack[-1] < stacks[i+1][-1])):
+                moves.add((i, 1))
+        
+    return moves
+
 def Run_Tower_Of_Hanoi(number_of_stacks, number_of_disks, number_of_iterations):
     # Initialize data structures
     stacks = [list() for i in range(number_of_stacks)]
     stacks[0] = [weight for weight in reversed(range(1, number_of_disks+1))]
     centers_of_mass = list()
     
+    
     # Loop over T iterations
     for t in range(1, number_of_iterations+1):
-    
-        # Build move set
+        print("")
+        print("T: ", t)
+        
+        moves = Get_Move_Set(stacks)
+        print(moves)
         
         # Randomly choose move from move set
+        move = random.choice(list(moves))
+        print(move)
+        
+        # Implement move
+        source_stack_index = move[0]
+        destination_stack_index = move[0] + move[1]
+        stacks[destination_stack_index].append(stacks[source_stack_index].pop())
         
         # Calculate and append center of mass to list
-        
-        print("T: ", t)
         
     # Return center of mass mean and std
     
